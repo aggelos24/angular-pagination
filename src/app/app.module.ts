288fb788e9156fbda825bd7ccdb8fbb695d06c02
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2018-04-24 19:14:54 +0300

    Module removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -14,8 +13,7 @@ import { AppComponent } from './app.component';
 	imports: [
 		BrowserModule,
 		HttpClientModule,
-		CommonModule,
-		FormsModule
+		CommonModule
 	],
 	providers: [],
 	bootstrap: [ AppComponent ]
