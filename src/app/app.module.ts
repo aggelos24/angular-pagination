50c10e207e81aab4230cc66016e7365055842860
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2018-06-13 13:06:29 +0300

    removed unneeded import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
@@ -12,8 +11,7 @@ import { AppComponent } from './app.component';
 	],
 	imports: [
 		BrowserModule,
-		HttpClientModule,
-		CommonModule
+		HttpClientModule
 	],
 	providers: [],
 	bootstrap: [ AppComponent ]
